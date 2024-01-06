f18209a977d12ee7bbddd2dce939d0dd8a6da314
Tony Dugué <duguetony@gmail.com>
2024-01-06 23:36:34 +0100

    Spring Boot initialization

---
+++ src/main/java/com/tonydugue/restapibookscollection/RestApiBooksCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.tonydugue.restapibookscollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBooksCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiBooksCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonydugue/restapibookscollection/RestApiBooksCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonydugue.restapibookscollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBooksCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
