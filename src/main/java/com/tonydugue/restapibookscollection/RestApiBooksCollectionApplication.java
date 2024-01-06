04ad6a64d7f38f3c6d60d5e04e5753c819637f0f
Tony Dugué <duguetony@gmail.com>
2024-01-07 00:36:16 +0100

    Databases (production & test) initialization with Docker

--- src/main/java/com/tonydugue/restapibookscollection/RestApiBooksCollectionApplication.java
+++ src/main/java/com/tonydugue/restapibookscollection/RestApiBooksCollectionApplication.java
@@ -1,13 +1,14 @@
 package com.tonydugue.restapibookscollection;
 
+import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log
 public class RestApiBooksCollectionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiBooksCollectionApplication.class, args);
 	}
-
 }
